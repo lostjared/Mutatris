9ea6a92e1d244f6a004dfd130b257d88c5546871
Jared Bruni <jaredbruni@gmail.com>
2024-10-23 03:58:41 -0700

    game object
    game object

---
+++ source/puzzle.cpp
@@ -0,0 +1,19 @@
+#include "puzzle.hpp"
+
+namespace obj {
+
+    PuzzleObject::~PuzzleObject() {
+
+    }
+    void PuzzleObject::draw(SDL_Renderer *renderer) {
+
+    }
+
+    void PuzzleObject::event(SDL_Event &e)  {
+
+    }
+    void PuzzleObject::load(SDL_Renderer *renderer) {
+
+    }
+    
+}
---
+++ source/puzzle.hpp
@@ -0,0 +1,24 @@
+#ifndef _PUZZLE_H__
+#define _PUZZLE_H__
+
+#include"game_object.hpp"
+#include"SDL.h"
+#include"SDL_ttf.h"
+#include"quadtris.hpp"
+
+namespace obj {
+
+    class PuzzleObject : public GameObject {
+    public:
+        PuzzleObject() = default;
+        virtual ~PuzzleObject();
+        virtual void draw(SDL_Renderer *renderer) override;
+        virtual void event(SDL_Event &e) override;
+        virtual void load(SDL_Renderer *renderer) override;
+    protected:
+        puzzle::PuzzleGame game;
+    };
+
+}
+
+#endif
